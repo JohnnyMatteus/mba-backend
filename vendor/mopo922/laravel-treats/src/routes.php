d0efd5d1fe93dc68eafbc58c75b59fdcdc8ba734
Johnny Santos <johnny.santos@sociisrh.com.br>
2022-04-07 21:29:55 -0300

    RESOLVENDO CONFLITOS

--- vendor/mopo922/laravel-treats/src/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web'], function () {
-    Route::get('terms', 'LaravelTreats\\Controller\\TermsController@getIndex');
-    Route::get('privacy', 'LaravelTreats\\Controller\\TermsController@getPrivacy');
-});
